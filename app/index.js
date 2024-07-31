fcbb9fd0e2fb83504975625c143e980c0555e98f
Nonso Anetoh <anet0001@algonquinlive.com>
2024-07-31 12:44:49 -0400

    adds: preloader html

--- app/index.js
+++ app/index.js
@@ -3,6 +3,9 @@ class App {
     this.elements = {};
     console.log("App Initialized");
   }
+
+  // navigation controls based on keyboard input and button controls
+  // animate sprite on active
 }
 
 document.addEventListener("DOMContentLoaded", () => {
